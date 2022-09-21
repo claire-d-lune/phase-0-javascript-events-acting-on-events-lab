41135ce872a082c7c85db92ce1a2e6f5876b960c
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-09-21 15:37:24 -0600

    commit tree 15c7766ab3a641c57e54bc000c25da94021cc3cf

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+const dodger = document.getElementById('dodger')
+
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+    if (event.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+    else if (event.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+  });
+
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftCoordinate = dodger.style.left.replace("px", "");
+    const leftCorner = parseInt(leftCoordinate, 10)
+    if (leftCorner < 380) {
+        dodger.style.left = `${leftCorner + 1}px`
+    }
+  }
